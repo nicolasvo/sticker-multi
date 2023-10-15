fd4c9a4c99d932524c571595227a4a550d863aa3
nico <nicolasvo95@gmail.com>
2023-10-15 23:18:25 +0700

    init sam

--- bot.py
+++ bot.py
@@ -36,6 +36,28 @@ def make_keyboard(message_id):
                 "Send sticker as file", callback_data=f"file_{message_id}"
             ),
         ],
+        [
+            InlineKeyboardButton(
+                "Write text prompt", callback_data=f"sam_{message_id}"
+            ),
+        ],
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    return reply_markup
+
+
+def make_keyboard_sam(message_id):
+    keyboard = [
+        [
+            InlineKeyboardButton("Yes", callback_data=f"yes_{message_id}"),
+            InlineKeyboardButton("No", callback_data=f"no_{message_id}"),
+        ],
+        [
+            InlineKeyboardButton(
+                "Try another text prompt",
+                callback_data=f"again_{message_id}",
+            ),
+        ],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
     return reply_markup
@@ -65,29 +87,41 @@ def request_rembg(input_path):
     return image_base64
 
 
+def request_gsa(input_path, text_prompt):
+    payload = {
+        "image": image_to_base64(input_path),
+        "text_prompt": text_prompt,
+    }
+    url = os.getenv("API_URL_GSA")
+    print("making request")
+    r = requests.post(url, json=payload, timeout=600)
+    print("request completed")
+    image_base64 = json.loads(r.content)["image"]
+    return image_base64
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text("Hi! I make stickers. Send me a picture 👨‍🎨")
 
 
 async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    bot = update.get_bot()
+    user = User(update)
+    user_message_id = f"{user.id}_{update.message.id}"
+    input_path = f"/tmp/{user_message_id}_input.jpeg"
+    output_path = f"/tmp/{user_message_id}_output.webp"
+    output_png_path = f"/tmp/{user_message_id}_output.png"
+    output_original_path = f"/tmp/{user_message_id}_output_original.webp"
     if update.message.photo:
         await update.message.reply_text("🧮")
-        user = User(update)
         print(user.sticker_set_name)
         print(user.sticker_set_title)
         reply_markup = make_keyboard(update.message.id)
-        user_message_id = f"{user.id}_{update.message.id}"
-        input_path = f"/tmp/{user_message_id}_input.jpeg"
-        output_path = f"/tmp/{user_message_id}_output.webp"
-        output_png_path = f"/tmp/{user_message_id}_output.png"
-        output_original_path = f"/tmp/{user_message_id}_output_original.webp"
-
         file_id = update.message.photo[-1].file_id
         media_message = await context.bot.get_file(file_id)
         await media_message.download_to_drive(input_path)
 
         try:
-            bot = update.get_bot()
             image_base64 = request_rembg(input_path)
             base64_to_image(image_base64, output_path)
             base64_to_image(image_base64, output_png_path)
@@ -107,23 +141,60 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> 
         except Exception as e:
             await update.message.reply_text(str(e))
     else:
-        await update.message.reply_text("Send me a picture 👨‍🎨")
+        import re
+
+        user = User(update)
+        directory = "/tmp/"
+        pattern = rf"^{user.id}_.*_input_sam\.jpeg$"
+        files = os.listdir(directory)
+        filtered_files = [file for file in files if re.match(pattern, file)]
+        filtered_files.sort(
+            key=lambda x: os.path.getmtime(os.path.join(directory, x)), reverse=True
+        )
+        print(filtered_files)
+        if len(filtered_files) > 0:
+            await update.message.reply_text("🪄")
+            input_sam_path = f"/tmp/{filtered_files[0]}"
+            image_base64 = request_gsa(input_sam_path, update.message.text)
+            base64_to_image(image_base64, output_path)
+            compress_image(output_path, output_path)
+            reply_markup = make_keyboard_sam(update.message.id)
+            await update.message.reply_text(
+                "Do you want to add this sticker? ✍️",
+            )
+            await bot.send_sticker(
+                user.id,
+                output_path,
+                reply_markup=reply_markup,
+            )
+            # if image_base64 == "":
+            #     await update.message.reply_text(f"'{update.message.text}' was not detected in the image.\nTry another prompt?")
+            #     return
+
+        else:
+            await update.message.reply_text("Send me a picture 👨‍🎨")
 
 
 async def handle_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Parses the CallbackQuery and updates the message text."""
     user = User(update)
     query = update.callback_query
-    await query.edit_message_reply_markup(reply_markup=None)
+    # await query.edit_message_reply_markup(reply_markup=None)
     await query.answer()
     user_message_id = f"{user.id}_{query.data.split('_')[-1]}"
     input_path = f"/tmp/{user_message_id}_input.jpeg"
+    input_sam_path = f"/tmp/{user_message_id}_input_sam.jpeg"
     output_path = f"/tmp/{user_message_id}_output.webp"
     output_png_path = f"/tmp/{user_message_id}_output.png"
     output_original_path = f"/tmp/{user_message_id}_output_original.webp"
     data = query.data.split("_")[0]
 
-    if data == "yes":
+    if data == "sam":
+        os.rename(input_path, input_sam_path)
+        await query.message.reply_text(
+            "Write what you want to cut.\nFor example: person left and brown cat 🖖"
+        )
+    elif data == "yes":
         await query.delete_message()
         await query.message.reply_text("Sticker added 👌")
         await add_sticker(
@@ -131,9 +202,11 @@ async def handle_choice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> N
             update.get_bot(),
             output_path,
         )
+        os.remove(input_sam_path) if os.path.exists(input_sam_path) else None
     elif data == "no":
         await query.delete_message()
         await query.message.reply_text("Sticker discarded 🤌")
+        os.remove(input_sam_path) if os.path.exists(input_sam_path) else None
     elif data == "original":
         if not os.path.exists(output_original_path):
             print(f"file {output_original_path} not found")
